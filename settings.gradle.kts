0c06ff7653328a67b03e06a66c665b46482c53e3
soywiz <soywiz@gmail.com>
2023-05-25 11:40:30 +0200

    Fix testing by using kproject 0.3.1 supporting testDependencies

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -36,7 +36,7 @@ pluginManagement {
 
 plugins {
     //id("com.soywiz.kproject.settings") version "0.0.1-SNAPSHOT"
-    id("com.soywiz.kproject.settings") version "0.3.0"
+    id("com.soywiz.kproject.settings") version "0.3.1"
 }
 
 //kproject("./deps")
